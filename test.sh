0679761e820cd91b15467ae4bffc4468ff3167d0
Luke Skibinski <l.skibinski@elifesciences.org>
2015-03-19 14:25:24 +0000

    split the downloading and unpacking into it's own script. removed the break if a failure occurs.

--- test.sh
+++ test.sh
@@ -1,29 +1,14 @@
 #!/bin/bash
-mkdir -p bucket && cd bucket
-#aws s3 sync s3://elife-ejp-poa-delivery .
 
-unzipdir=unzipped
+if [ "$1" -eq "" ]; then
+    echo "Usage: ./test.sh <pdfdir>"
+    exit 1;
+fi;
 
-#rm -f $unzipdir/*
-#rmdir $unzipdir
-
-# extract the _merged_ pdf from the zip file
-for file in `ls *.zip`; do
-    # test integrity of zip file, skip if it looks bad
-    if ! unzip -t $file 2>/dev/null 1>/dev/null; then
-        echo "broken zip: $file"
-        continue
-    fi
-    
-    unzip -o -B $file *_merged_* -d $unzipdir 2>/dev/null 1>/dev/null
-    
-done
-
-# test the pdfs
-for pdffile in `ls $unzipdir/*.pdf`; do
-    if ! sh ../strip-coverletter.sh $pdffile; then 
-        echo "failure in $pdffile"
-        break
+for pdffile in `ls $1/*.pdf`; do
+    if ! sh strip-coverletter.sh $pdffile; then 
+        echo "FAILURE in $pdffile"
+        #break
     fi
     echo ""
 done;
