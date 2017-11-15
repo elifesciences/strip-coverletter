1fcd949d77818beeb99b0cd7b7cde545205af8da
Luke Skibinski <l.skibinski@elifesciences.org>
2017-11-15 14:14:24 +1030

    upgraded pdfsam-console commands to sejda-console commands

--- strip-coverletter.sh
+++ strip-coverletter.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 description="script that detects and removes the leading cover sheet from the 
              special article PDF sent to peer reviewers"
 
@@ -16,7 +18,7 @@ if [ ! -f /usr/bin/pdftotext ]; then
     exit 1
 fi
 
-if [ ! -f /usr/bin/pdfsam-console ]; then
+if ! type -P pdfsam-console; then
     errcho "'pdfsam-console' not found."
     errcho "For Ubuntu, follow installation directions:"
     errcho "   http://www.sysads.co.uk/2014/08/install-pdfsam-2-2-4-on-ubuntu-14-04/"
@@ -39,7 +41,7 @@ echo "exploding to:" $explodeddir
 mkdir -p $explodeddir
 
 echo 'exploding pdf to individual files...'
-pdfsam-console -f $1 -o $explodeddir -S BURST -overwrite split > $explodeddir/log
+pdfsam-console simplesplit --files $1 --output $explodeddir --existingOutput overwrite --predefinedPages all > $explodeddir/log
 
 echo 'looking for non-cover pages...'
 ncp=-1 # non-cover page
@@ -74,10 +76,10 @@ else
     pathargs=""
     #echo "endofcoverletter='$i' totalpages='$total_pages'"
     while [ "$i" -le "$total_pages" ]; do
-        pathargs="$pathargs -f $explodeddir/$i\_$pdf"
+        pathargs="$pathargs $explodeddir/$i\_$pdf"
         i=$(( $i + 1 ))
     done
-    cmd="pdfsam-console -overwrite -o $output_pdf $pathargs concat > /dev/null"
+    cmd="pdfsam-console merge --files $pathargs --output $output_pdf --overwrite > /dev/null"
     eval $cmd
 fi
 
