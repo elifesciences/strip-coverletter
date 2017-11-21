eb2a07b7c9443fa73142ce4512bd6ace51511a86
Luke Skibinski <l.skibinski@elifesciences.org>
2017-11-21 18:47:43 +1030

    improved test script expects a directory called bucket-pdf with the pdf files extracted from poa zips (see unzip-merged.sh)

--- test.sh
+++ test.sh
@@ -1,16 +1,40 @@
 #!/bin/bash
+# calls strip-coverletter.sh on all pdf files in given directory
+# there shouldn't be any failures
 
-if [ "$1" -eq "" ]; then
+set -e
+trap ctrl_c INT
+
+function ctrl_c() {
+    echo "ctrl-c caught"
+    exit 1
+}
+
+#
+
+pdfdir=$1
+if [ -z "$pdfdir" ]; then
     echo "Usage: ./test.sh <pdfdir>"
-    exit 1;
-fi;
+    exit 1
+fi
 
-rm "$1/*.decap"
+outdir="$pdfdir-decap"
+mkdir -p $outdir
 
-for pdffile in `ls $1/*.pdf`; do
-    if ! sh strip-coverletter.sh $pdffile "$pdffile.decap"; then 
-        echo "FAILURE in $pdffile"
-        #break
+#
+
+for pdffile in `ls $pdfdir/*.pdf`; do
+    fname=${pdffile##*/}
+    in=$pdffile
+    out="$outdir/$fname"
+    if [ -e $out ]; then
+        echo "found $out, skipping"
+        continue
     fi
-    echo ""
-done;
+    if ! ./strip-coverletter.sh $in $out; then
+        echo "====================="
+        echo "FAILURE with: ./strip-coverletter.sh $in $out"
+        cat /tmp/$fname-exploded/log
+        echo "====================="
+    fi
+done
