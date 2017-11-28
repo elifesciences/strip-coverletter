de9102d36137a8b16b67e7bba25a643c29f9b05f
Luke Skibinski <l.skibinski@elifesciences.org>
2017-11-28 16:51:29 +1030

    any existing sejda-console symlink is overwritten when downloading

--- download-sejda.sh
+++ download-sejda.sh
@@ -2,5 +2,5 @@
 set -e
 wget --quiet https://github.com/torakiki/sejda/releases/download/v3.2.38/sejda-console-3.2.38-bin.zip
 unzip -q sejda-console-3.2.38-bin.zip 
-ln -s sejda-console-3.2.38 sejda-console
+ln -sf sejda-console-3.2.38 sejda-console
 rm sejda-console-3.2.38-bin.zip
