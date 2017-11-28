ca6ac123fcb80647533bfeef3a672fcb57522e93
Luke Skibinski <l.skibinski@elifesciences.org>
2017-11-28 14:16:57 +1030

    download-sejda script now removes download afterwards

--- download-sejda.sh
+++ download-sejda.sh
@@ -3,3 +3,4 @@ set -e
 wget --quiet https://github.com/torakiki/sejda/releases/download/v3.2.38/sejda-console-3.2.38-bin.zip
 unzip -q sejda-console-3.2.38-bin.zip 
 ln -s sejda-console-3.2.38 sejda-console
+rm sejda-console-3.2.38-bin.zip
