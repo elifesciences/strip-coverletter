7b41a896c4d14497042f1c0e83a86bde768dc8ee
Luke Skibinski <l.skibinski@elifesciences.org>
2017-11-28 12:54:29 +1030

    project_tests.sh no longer expects caller to be root

--- project_tests.sh
+++ project_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-docker run -v $(pwd):/data -u $(id -u $(whoami)) strip-coverletter /data/10012_1_merged_1431494755.pdf /data/out.pdf
+sudo docker run -v $(pwd):/data -u $(id -u $(whoami)) strip-coverletter /data/10012_1_merged_1431494755.pdf /data/out.pdf
