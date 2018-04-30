2dfd4bcb665ae77e0ad3d4c55ac5e63fc986d1a7
Luke Skibinski <l.skibinski@elifesciences.org>
2018-04-30 12:21:18 +0930

    introduces early exit from downsample.sh if potential errors exist

--- downsample.sh
+++ downsample.sh
@@ -19,6 +19,35 @@ function finish {
 }
 trap finish EXIT
 
+# reads stdout from `gs` and looks for error strings
+# exits with status 2 if one detected
+# strip-coverletter.sh catches any errors and will prefer decap over squashed
+function detect_errors {
+    while read line; do
+        echo "$line"
+
+        # reset error
+        error=""
+
+        # "**** Error reading a content stream. The page may be incomplete."
+        if [[ "$line" == "**** Error: "* ]]; then
+            error=$line
+        fi
+
+        # "Output may be incorrect."
+        if [ "$line" = "Output may be incorrect." ]; then
+            error=$line
+        fi
+
+        # if the value of error is not empty, fail
+        if [ ! -z "$error" ]; then
+            echo ""
+            echo "detected imperfect squash: $error"
+            exit 2
+        fi
+    done
+}
+
 # these don't seem to help much/at all
 #   -dNumRenderingThreads=64 \
 #   -dBandBufferSpace=500000000 \
@@ -27,7 +56,7 @@ trap finish EXIT
 
 timeout --preserve-status $duration \
     gs \
-       -o $outfile \
+       -o "$outfile" \
        -sDEVICE=pdfwrite \
        -dDownsampleColorImages=true \
        -dDownsampleGrayImages=true \
@@ -39,4 +68,4 @@ timeout --preserve-status $duration \
        -dGrayImageDownsampleThreshold=$threshold \
        -dMonoImageDownsampleThreshold=$threshold \
        -dAutoRotatePages=/None \
-       $infile
+       "$infile" | detect_errors
