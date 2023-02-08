c61f433c7dab769695f157e80ee114a91f49caca
Luke <lsh-0@users.noreply.github.com>
2023-02-08 13:39:17 +1030

    test.sh, fixes reference to 'test' directory, which is now variable. linted with shellcheck

--- test.sh
+++ test.sh
@@ -12,29 +12,28 @@ function testdecap {
     expected_rc=$1
     pdf_dir="$test_fixture_dir/$2" # /ext/cached-repositories/strip-coverletter-test-fixtures/working-fixtures
     
-    for pdffile in `ls $pdf_dir/*.pdf`; do
+    for pdffile in "$pdf_dir"/*.pdf; do
         pdffile_bname=${pdffile##*/} # tests/working-fixtures/foo.pdf => foo.pdf
         set +e
         echo "testing $pdffile"
-        ./strip-coverletter.sh $pdffile tmp.pdf &> /dev/null
+        ./strip-coverletter.sh "$pdffile" tmp.pdf &> /dev/null
         rc=$?
         set -e
-        if [ $rc -ne $expected_rc ]; then
-            # assertion failed.
-            echo "got return code '$rc', expected $expected_rc for fixture $pdffile"
-        else
+        if [ "$rc" -eq "$expected_rc" ]; then
             # assertion succeeded. 
             # remove the dump file (written to same dir as output file), if it exists.
             rm -f "$pdffile_bname.dump.tar"
+        else
+            # assertion failed.
+            echo "got return code '$rc', expected $expected_rc for fixture $pdffile"
         fi
-
         # whatever the case, remove the output file if it exists
         rm -f tmp.pdf
     done
 }
 
-if [ ! -d tests ]; then
-    echo "test fixtures directory not found"
+if [ ! -d "$test_fixture_dir" ]; then
+    echo "test fixture directory not found: $test_fixture_dir"
     echo "see ./download-test-fixtures.sh"
     exit 1
 fi
