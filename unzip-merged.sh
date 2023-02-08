69dd94146ec5ad62ca3250e5d46246ef77f160c9
Luke <lsh-0@users.noreply.github.com>
2023-02-08 12:59:54 +1030

    unzip-merged, skips unzipping if 'merged' pdf file already exists

--- unzip-merged.sh
+++ unzip-merged.sh
@@ -7,17 +7,20 @@ pdfdir="$bucketdir-pdf"
 mkdir -p $bucketdir $pdfdir
 
 for pdffile in `ls $bucketdir/*.zip`; do
-    merged=$(zipinfo -1 $pdffile | grep merged) || echo "bad zip or a 'merged' pdf not found: $pdffile"
+    echo "$pdffile -> $pdfdir/$merged"
+    merged=$(zipinfo -1 $pdffile | grep merged) || echo "- bad zip or a 'merged' pdf not found."
     if [ -z $merged ]; then
         continue
     fi
-
-    echo "$pdffile -> $pdfdir/$merged"
+    if [ -f "$pdfdir/$merged" ]; then
+        echo "- skipping, 'merged' pdf exists."
+        continue
+    fi
     # -o  overwrite existing files without prompting
     # -u  update existing files and create new ones if needed
     # -q  quiet
     unzip -q -o -u $pdffile $merged -d "$pdfdir/"
-    test -e $pdfdir/$merged || echo "failed to find merge file $pdfdir/$merged"
+    test -e $pdfdir/$merged || echo "- failed to find 'merged' pdf to strip."
 done
 
 echo "done, now run:
